204c3a291aa35bfe77abfbe3a02f565da36f2a2c
Zaid Bin Rashid <zaidbinrashid85@gmail.com>
2025-11-23 22:27:04 +0530

    Added Cli Functionality

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-import express from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app = express();
-const PORT = process.env.PORT;
+#!/usr/bin/env node
+// Above line is called Shebang Line which tells the OS which interpreter to use.
 
 
-app.get('/', (req, res) =>{
-    res.send("TMDB CLI Tool");
-})
 
-app.listen(PORT,() => {
-    console.log(`erver running on ${PORT}`);
-})
+const url = 'https://api.themoviedb.org/3/authentication';
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiODk4NjAwYWM1ZDIwOTA4ZmIzZDk5NjU3YzUzYjE1NyIsIm5iZiI6MTc2MzkxNTU4Ny41OTgsInN1YiI6IjY5MjMzNzQzM2UyZGJhN2NiOTk1OGExMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.HDN7rIs7RMGgpPpdm0D2TuTasLvyc5tWX8LN35gIdSo'
+  }
+};
+
+fetch(url, options)
+  .then(res => res.json())
+  .then(json => console.log(json))
+  .catch(err => console.error(err));
